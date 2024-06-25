b18b148f8da9dc83fb932221f6b3f3886b18c44e
dfguerrerom <dfgm2006@gmail.com>
2024-06-25 16:34:00 +0200

    refactor(directory): allow parents creation

--- component/parameter/directory.py
+++ component/parameter/directory.py
@@ -4,10 +4,10 @@ from pathlib import Path
 # please make sure that any result that you produce is embeded inside this folder
 # create a folder adapted to your need inside this folder to save anything in sepal
 module_dir = Path.home() / "module_results"
-module_dir.mkdir(exist_ok=True)
+module_dir.mkdir(exist_ok=True, parents=True)
 
 weplan_dir = module_dir / "weplan"
-weplan_dir.mkdir(exist_ok=True)
+weplan_dir.mkdir(exist_ok=True, parents=True)
 
 tmp_dir = Path.home() / "tmp" / "weplan"
-tmp_dir.mkdir(exist_ok=True)
+tmp_dir.mkdir(exist_ok=True, parents=True)
